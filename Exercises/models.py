a03cbe50983f336fa8246528f348e1a474f44469
andre-cloud <55535285+andre-cloud@users.noreply.github.com>
2020-10-08 12:14:33 +0200

    added a small comment below each entry for a new exercise

--- Exercises/models.py
+++ Exercises/models.py
@@ -6,12 +6,12 @@ from polymorphic.models import PolymorphicModel
 
 class Exercise(PolymorphicModel):
 
-    title = models.CharField(max_length=120)
-    text = models.TextField()
+    title = models.CharField(max_length=120, help_text='Inserire un titolo esplicativo')
+    text = models.TextField(help_text='Scrivere il testo del problema. Se si è utilizzato il bot che sfrutta la tecnologia OCR controllare la correttezza del teso ricevuto.')
     date = models.DateTimeField(auto_now=False, auto_now_add=True)
-    referance = models.TextField()
+    referance = models.TextField(help_text='Se è un libro: Cognome N., Titolo del testo, Casa editrice, anno di pubblicazione con edizione, pagina. Se è un sito inserire il link')
     status = models.CharField(max_length=10, default="Draft", editable=False)
-    slug = models.SlugField() #human-friendly URL
+    slug = models.SlugField(help_text='NON MODIFICARE') #human-friendly URL
 
     def __str__(self) -> str:
         return self.title
@@ -23,9 +23,12 @@ class Matematica(Exercise):
         matrici = 'MAT', ('Matrici')
         stu_func = 'FUN', ('Studio di funzioni')
 
-    argomento = models.CharField(max_length=3, choices=Argomento.choices, default=Argomento.matrici)
-    dim = models.TextField()
-    grafico = models.FileField(blank=True)
+    argomento = models.CharField(max_length=3, choices=Argomento.choices, help_text='Selezionare l\'argomento di appartenza dell\'esercizio')
+    grafico = models.FileField(blank=True, help_text='Caricare un file .png/.svg/.jpeg contenente l\'eventuale grafico necessario per la dimostrazione')
+    dim = models.TextField(help_text='Scrivere la dimostrazione per risolvere il problema. È pensato anche come entry per definire l\'eventuale sistema di equazioni per risolvere il problema.')
+    commento = models.TextField(help_text='Commentare il processo mentale che ha portato alla scelta di ciò che è stato caricato come "struttura" e nell\'eventuale meccanismo.')
+    risultato = models.TextField(help_text='Evidenziare il risultato ottenuto dalla risoluzione delle equazioni.', blank=True)
+
 
     def __str__(self) -> str:
         return "Matematica"
@@ -44,10 +47,10 @@ class Chimica_Organica(Exercise):
         carbonile = 'RCO', ('Carbonile')
         sintesi = 'SIN', ('Sintesi')
     
-    argomento = models.CharField(max_length=3, choices=Argomento.choices, default=Argomento.sintesi)
-    strutture = models.FileField()
-    meccanismo = models.FileField(blank=True)
-    commento = models.TextField()
+    argomento = models.CharField(max_length=3, choices=Argomento.choices, help_text='Selezionare l\'argomento di appartenza dell\'esercizio')
+    strutture = models.FileField(help_text='Caricare un file .png/.svg/.jpeg contenente lo schema della reazione')
+    meccanismo = models.FileField(blank=True, help_text='Caricare un file .png/.svg/.jpeg contenente lo schema dell\'eventuale meccanismo di reazione')
+    commento = models.TextField(help_text='Commentare il processo mentale che ha portato alla scelta di ciò che è stato caricato come "struttura" e nell\'eventuale meccanismo.')
 
     def __str__(self) -> str:
         return "Chimica Organica"
@@ -58,15 +61,20 @@ class Chimica_Organica(Exercise):
 class Chimica_Generale_Analitica(Exercise):
 
     class Argomento(models.TextChoices):
-        elettrochimica = 'ELE', ('Elettrochimica')
-        redox = 'RED', ('Reazioni redox')
-        theo_atomica = 'ATM', ('Teoria Atomica e Proprietà Periodiche')
-        acidi_basi = 'ACB', ('Teorie del pH e tamponi')
+        theo_atomica = 'ATM', ('1. Teoria atomica e Proprietà periodiche')
+        reactions = 'REA', ('2. Reazioni chimiche')
+        stechio = 'STE', ('3. Stechiometria')
+        prin_therm = 'PTH', ('4. Principi di termochimica')
+        bond = 'BON', ('5. Legami chimici')
+        soluzioni = 'SOL', ('6. Soluzioni e concentrazioni')
+        colligative = 'COL', ('7. Proprietà colligative')
+        acidi_basi = 'ACB', ('8. Teorie del pH e tamponi')
+        redox = 'RED', ('9. Reazioni redox')
 
-    argomento = models.CharField(max_length=3, choices=Argomento.choices, default=Argomento.acidi_basi)
-    sistema = models.TextField()
-    commento = models.TextField()
-    risultato = models.TextField()
+    argomento = models.CharField(max_length=3, choices=Argomento.choices, help_text='Selezionare l\'argomento di appartenza dell\'esercizio')
+    sistema = models.TextField(help_text='Scrivere qui le varie equazioni utilizzate per risolvere il problema. Possibilmente numerarle.')
+    commento = models.TextField(help_text='Commentare il processo mentale che ha portato alla scrittura delle varie equazioni utilizzate facendo riferminto.')
+    risultato = models.TextField(help_text='Evidenziare il risultato ottenuto dalla risoluzione delle equazioni.')
 
     def __str__(self) -> str:
         return "Chimica Generale e Analitica"
@@ -77,14 +85,15 @@ class Chimica_Generale_Analitica(Exercise):
 class Chimica_Fisica(Exercise):
 
     class Argomento(models.TextChoices):
-        elettrodi = 'ELE', ('Elettrodi')
         gas = 'GAS', ('Teorie dei gas')
         termodinamica = 'THM', ('Termodinamica e termochimica')
+        cinetica = 'KIN', ('Cinetica chimica')
+        elettrodi = 'ELE', ('Elettrodi')
 
-    argomento = models.CharField(max_length=3, choices=Argomento.choices, default=Argomento.gas)
-    sistema = models.TextField()
-    commento = models.TextField()
-    risultato = models.TextField()
+    argomento = models.CharField(max_length=3, choices=Argomento.choices, help_text='Selezionare l\'argomento di appartenza dell\'esercizio')
+    sistema = models.TextField(help_text='Scrivere qui le varie equazioni utilizzate per risolvere il problema. Possibilmente numerarle.')
+    commento = models.TextField(help_text='Commentare il processo mentale che ha portato alla scrittura delle varie equazioni utilizzate facendo riferminto.')
+    risultato = models.TextField(help_text='Evidenziare il risultato ottenuto dalla risoluzione delle equazioni.')
 
     def __str__(self) -> str:
         return "Chimica Fisica"
