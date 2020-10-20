e33927e5766c03154d654b2b4237b94976fe5947
andre-cloud <55535285+andre-cloud@users.noreply.github.com>
2020-10-20 11:58:03 +0200

    Debug false

--- ChemSite/settings.py
+++ ChemSite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w#v9rehjm%h-)*3$%lh7hlvv)=sndwi@pl6031%u&v5k7)3ws6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
